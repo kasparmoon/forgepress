6ffaa09b3d2452054453a28396a5b944da443660
Abu Saeed Mohammad Sayem <abusaeedmohammadsayem@mac.mynetworksettings.com>
2025-07-16 15:06:17 -0400

    fix(posts): Style and correctly place the edit post link

--- template-parts/content-single.php
+++ template-parts/content-single.php
@@ -15,11 +15,19 @@
 
 	<header class="entry-header">
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-	</header>
-
-	<?php get_template_part( 'template-parts/entry-meta' ); ?>
+	</header><?php get_template_part( 'template-parts/entry-meta' ); ?>
 
 	<div class="entry-content">
-		<?php the_content(); ?>
-	</div>
-</article>
+		<?php
+		the_content();
+
+		wp_link_pages(
+			array(
+				'before' => '<div class="page-links">' . __( 'Pages:', 'forgepress' ),
+				'after'  => '</div>',
+			)
+		);
+		?>
+	</div><footer class="entry-footer">
+		<?php edit_post_link( esc_html__( 'Edit', 'forgepress' ), '<span class="edit-link">', '</span>' ); ?>
+	</footer></article>
