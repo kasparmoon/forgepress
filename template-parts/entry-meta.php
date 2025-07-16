1c7190518cbe656b7d3df3b3f2c624ec56145abe
Abu Saeed Mohammad Sayem <abusaeedmohammadsayem@mac.mynetworksettings.com>
2025-07-16 16:53:10 -0400

    fix(template): Restore post meta and finalize single post template

--- template-parts/entry-meta.php
+++ template-parts/entry-meta.php
@@ -10,7 +10,8 @@
 	<span class="posted-on">
 		<?php
 		printf(
-			'Posted on %s',
+			/* translators: %s: post date. */
+			esc_html__( 'Posted on %s', 'forgepress' ),
 			'<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . esc_html( get_the_date() ) . '</a>'
 		);
 		?>
@@ -18,9 +19,10 @@
 	<span class="byline">
 		<?php
 		printf(
-			'by %s',
+			/* translators: %s: post author. */
+			esc_html__( 'by %s', 'forgepress' ),
 			'<a href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a>'
 		);
 		?>
 	</span>
-</div>```
+</div>
