ac2097490b7112be4b4bd6db0af575513877694a
Abu Saeed Mohammad Sayem <abusaeedmohammadsayem@mac.mynetworksettings.com>
2025-07-16 16:32:57 -0400

    refactor(template): Simplify single.php and remove content-single partial

--- single.php
+++ single.php
@@ -15,20 +15,55 @@ get_header(); ?>
 			<?php
 			while ( have_posts() ) :
 				the_post();
+				?>
+				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+					<?php if ( has_post_thumbnail() ) : ?>
+						<div class="post-thumbnail">
+							<?php the_post_thumbnail( 'large' ); ?>
+						</div>
+					<?php endif; ?>
 
-				get_template_part( 'template-parts/content', 'single' ); // We will create this file next
+					<header class="entry-header">
+						<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+					</header><?php get_template_part( 'template-parts/entry-meta' ); ?>
 
+					<div class="entry-content">
+						<?php
+						the_content(
+							sprintf(
+								wp_kses(
+									/* translators: %s: Name of current post. Only visible to screen readers */
+									__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'forgepress' ),
+									array(
+										'span' => array(
+											'class' => array(),
+										),
+									)
+								),
+								wp_kses_post( get_the_title() )
+							)
+						);
+
+						wp_link_pages(
+							array(
+								'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'forgepress' ),
+								'after'  => '</div>',
+							)
+						);
+						?>
+					</div><footer class="entry-footer">
+						<?php edit_post_link( esc_html__( 'Edit', 'forgepress' ), '<span class="edit-link">', '</span>' ); ?>
+					</footer></article><?php
+
+				// If comments are open or there is at least one comment, load up the comment template.
 				if ( comments_open() || get_comments_number() ) :
 					comments_template();
 				endif;
 
-			endwhile;
+			endwhile; // End of the loop.
 			?>
 
-		</main>
-	</div>
-
-	<?php get_sidebar(); ?>
+		</main></div><?php get_sidebar(); ?>
 	<?php get_sidebar( 'secondary' ); ?>
 </div>
 
