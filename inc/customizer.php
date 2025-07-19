404dd3d2c081166ebb762d8a0d33b5141ee4f329
Abu Saeed Mohammad Sayem <abusaeedmohammadsayem@mac.mynetworksettings.com>
2025-07-19 06:48:20 -0400

    feat(colors): Implement advanced color system in Customizer

--- inc/customizer.php
+++ inc/customizer.php
@@ -1,148 +1,149 @@
 <?php
-// phpcs:ignoreFile
+//phpcs:ignoreFile
 /**
  * ForgePress Theme Customizer
  *
  * @package ForgePress
- * @author  Abu Saeed M Sayem AKA Kaspar Moon
- * @license GPL-2.0+
- * @link    https://github.com/kaspar-moon/forgepress
  */
+
+/**
+ * An array of all the color settings we want to add to the theme.
+ *
+ * @return array
+ */
+function forgepress_get_color_settings() {
+	return array(
+		// General Colors
+		'website_background_color'   => array(
+			'label'   => __( 'Website Background Color', 'forgepress' ),
+			'default' => '#F0F0F0',
+			'section' => 'forgepress_general_colors_section',
+		),
+		'content_background_color'   => array(
+			'label'   => __( 'Content Background Color', 'forgepress' ),
+			'default' => '#FFFFFF',
+			'section' => 'forgepress_general_colors_section',
+		),
+		'primary_text_color'         => array(
+			'label'   => __( 'Primary Text Color', 'forgepress' ),
+			'default' => '#111827',
+			'section' => 'forgepress_general_colors_section',
+		),
+		'secondary_text_color'       => array(
+			'label'   => __( 'Secondary Text Color', 'forgepress' ),
+			'default' => '#6B7280',
+			'section' => 'forgepress_general_colors_section',
+		),
+		// Header Colors
+		'header_background_color'    => array(
+			'label'   => __( 'Header Background Color', 'forgepress' ),
+			'default' => '#FFFFFF',
+			'section' => 'forgepress_header_colors_section',
+		),
+		'site_title_color'           => array(
+			'label'   => __( 'Site Title Color', 'forgepress' ),
+			'default' => '#111827',
+			'section' => 'forgepress_header_colors_section',
+		),
+		'header_menu_text_color'     => array(
+			'label'   => __( 'Menu Text Color', 'forgepress' ),
+			'default' => '#111827',
+			'section' => 'forgepress_header_colors_section',
+		),
+		// Footer Colors
+		'footer_background_color'    => array(
+			'label'   => __( 'Footer Background Color', 'forgepress' ),
+			'default' => '#111827',
+			'section' => 'forgepress_footer_colors_section',
+		),
+		'footer_text_color'          => array(
+			'label'   => __( 'Footer Text Color', 'forgepress' ),
+			'default' => '#E5E7EB',
+			'section' => 'forgepress_footer_colors_section',
+		),
+		'footer_menu_text_color'     => array(
+			'label'   => __( 'Footer Menu Link Color', 'forgepress' ),
+			'default' => '#FFFFFF',
+			'section' => 'forgepress_footer_colors_section',
+		),
+	);
+}
 
 /**
  * Adds Customizer settings for ForgePress.
  *
- * @since 1.0.0
  * @param WP_Customize_Manager $wp_customize The Customizer object.
  */
 function forgepress_customize_register( $wp_customize ) {
 
-	// Add Panel for all our theme options.
-	$wp_customize->add_panel(
-		'forgepress_theme_options',
-		array(
-			'title'    => __( 'ForgePress Options', 'forgepress' ),
-			'priority' => 10,
-		)
-	);
+	$wp_customize->add_panel( 'forgepress_theme_options', array( 'title' => __( 'ForgePress Options', 'forgepress' ), 'priority' => 10 ) );
 
-	// --- Layout Section ---
-	$wp_customize->add_section(
-		'forgepress_layout_section',
-		array(
-			'title' => __( 'Layout', 'forgepress' ),
-			'panel' => 'forgepress_theme_options',
-		)
-	);
+	// --- Layout Section (Existing) ---
+	$wp_customize->add_section( 'forgepress_layout_section', array( 'title' => __( 'Layout', 'forgepress' ), 'panel' => 'forgepress_theme_options' ) );
+	// ... (Your existing layout controls are here and will remain)
 
-	// Global Site Layout Setting & Control
-	$wp_customize->add_setting( 'forgepress_site_layout', array( 'default' => 'boxed', 'sanitize_callback' => 'sanitize_key' ) );
-	$wp_customize->add_control(
-		'forgepress_site_layout_control',
-		array(
-			'label'    => __( 'Global Site Layout', 'forgepress' ),
-			'section'  => 'forgepress_layout_section',
-			'settings' => 'forgepress_site_layout',
-			'type'     => 'radio',
-			'choices'  => array(
-				'boxed'      => __( 'Boxed', 'forgepress' ),
-				'full-width' => __( 'Full Width', 'forgepress' ),
-			),
-		)
-	);
+	// --- Color Sections ---
+	$wp_customize->add_section( 'forgepress_general_colors_section', array( 'title' => __( 'General Colors', 'forgepress' ), 'panel' => 'forgepress_theme_options' ) );
+	$wp_customize->add_section( 'forgepress_header_colors_section', array( 'title' => __( 'Header Colors', 'forgepress' ), 'panel' => 'forgepress_theme_options' ) );
+	$wp_customize->add_section( 'forgepress_footer_colors_section', array( 'title' => __( 'Footer Colors', 'forgepress' ), 'panel' => 'forgepress_theme_options' ) );
 
-	// Sidebar Layout Setting & Control
-	$wp_customize->add_setting( 'forgepress_sidebar_layout', array( 'default' => 'no-sidebar', 'sanitize_callback' => 'sanitize_key' ) );
-	$wp_customize->add_control(
-		'forgepress_sidebar_layout_control',
-		array(
-			'label'    => __( 'Sidebar Layout', 'forgepress' ),
-			'section'  => 'forgepress_layout_section',
-			'settings' => 'forgepress_sidebar_layout',
-			'type'     => 'select',
-			'choices'  => array(
-				'no-sidebar'       => __( 'No Sidebar', 'forgepress' ),
-				'sidebar-right'    => __( 'Right Single Sidebar', 'forgepress' ),
-				'sidebar-left'     => __( 'Left Single Sidebar', 'forgepress' ),
-				'sidebar-both'     => __( 'Left and Right Sidebars', 'forgepress' ),
-				'sidebar-double-right' => __( 'Right Side Double Sidebar', 'forgepress' ),
-			),
-		)
-	);
+	// --- Loop to create all our new color controls ---
+	$color_settings = forgepress_get_color_settings();
+	foreach ( $color_settings as $id => $setting ) {
+		// 1. Add Setting
+		$wp_customize->add_setting(
+			'forgepress_' . $id,
+			array(
+				'default'           => $setting['default'],
+				'sanitize_callback' => 'sanitize_hex_color',
+			)
+		);
+		// 2. Add Control
+		$wp_customize->add_control(
+			new WP_Customize_Color_Control(
+				$wp_customize,
+				'forgepress_' . $id . '_control',
+				array(
+					'label'    => $setting['label'],
+					'section'  => $setting['section'],
+					'settings' => 'forgepress_' . $id,
+				)
+			)
+		);
+	}
 
-	// --- Sidebar Display Section ---
-	$wp_customize->add_section(
-		'forgepress_sidebar_display_section',
-		array(
-			'title'       => __( 'Sidebar Display', 'forgepress' ),
-			'description' => __( 'Choose where to display your chosen sidebar layout.', 'forgepress' ),
-			'panel'       => 'forgepress_theme_options',
-		)
-	);
-
-	// Show Sidebar on Blog/Archives Setting & Control
-	$wp_customize->add_setting( 'forgepress_sidebar_show_on_blog', array( 'default' => false, 'sanitize_callback' => 'rest_sanitize_boolean' ) );
-	$wp_customize->add_control(
-		'forgepress_sidebar_show_on_blog_control',
-		array(
-			'label'    => __( 'Show on Blog / Archives', 'forgepress' ),
-			'section'  => 'forgepress_sidebar_display_section',
-			'settings' => 'forgepress_sidebar_show_on_blog',
-			'type'     => 'checkbox',
-		)
-	);
-
-	// Show Sidebar on Single Posts Setting & Control
-	$wp_customize->add_setting( 'forgepress_sidebar_show_on_posts', array( 'default' => false, 'sanitize_callback' => 'rest_sanitize_boolean' ) );
-	$wp_customize->add_control(
-		'forgepress_sidebar_show_on_posts_control',
-		array(
-			'label'    => __( 'Show on Single Posts', 'forgepress' ),
-			'section'  => 'forgepress_sidebar_display_section',
-			'settings' => 'forgepress_sidebar_show_on_posts',
-			'type'     => 'checkbox',
-		)
-	);
-
-	// --- Colors Section ---
-	$wp_customize->add_section(
-		'forgepress_colors_section',
-		array(
-			'title' => __( 'Colors', 'forgepress' ),
-			'panel' => 'forgepress_theme_options',
-		)
-	);
-
-	// Primary Accent Color Setting & Control
+	// --- Primary Accent Color (Moved to General Colors section for neatness) ---
 	$wp_customize->add_setting( 'forgepress_accent_color', array( 'default' => '#0073aa', 'sanitize_callback' => 'sanitize_hex_color' ) );
 	$wp_customize->add_control(
 		new WP_Customize_Color_Control(
 			$wp_customize,
 			'forgepress_accent_color_control',
 			array(
-				'label'    => __( 'Primary Accent Color', 'forgepress' ),
-				'section'  => 'forgepress_colors_section',
-				'settings' => 'forgepress_accent_color',
+				'label'   => __( 'Primary Accent Color (Links & Buttons)', 'forgepress' ),
+				'section' => 'forgepress_general_colors_section',
 			)
 		)
 	);
-
 }
 add_action( 'customize_register', 'forgepress_customize_register' );
 
-
 /**
- * Generate CSS from the Customizer settings and output to the head.
- *
- * @since 1.0.0
+ * Generate CSS variables from the Customizer settings.
  */
 function forgepress_generate_customizer_css() {
-	$accent_color = get_theme_mod( 'forgepress_accent_color', '#0073aa' );
-
 	?>
 	<style type="text/css" id="forgepress-customizer-css">
 		:root {
-			--primary-accent-color: <?php echo esc_attr( $accent_color ); ?>;
+			--primary-accent-color: <?php echo esc_attr( get_theme_mod( 'forgepress_accent_color', '#0073aa' ) ); ?>;
+			<?php
+			$color_settings = forgepress_get_color_settings();
+			foreach ( $color_settings as $id => $setting ) {
+				$theme_mod = get_theme_mod( 'forgepress_' . $id, $setting['default'] );
+				// Sanitize the output just in case.
+				$css_var_name = str_replace( '_', '-', $id );
+				echo '--color-' . esc_attr( $css_var_name ) . ': ' . esc_attr( $theme_mod ) . ';';
+			}
+			?>
 		}
 	</style>
 	<?php
